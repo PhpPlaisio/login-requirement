047e6fb747380817a9d11a9af235c231871e4a9d
Paul Water <p.r.water@setbased.nl>
2017-12-12 12:49:19 +0100

    Modified the return type and updated the comments.

--- src/LoginRequirement.php
+++ src/LoginRequirement.php
@@ -9,12 +9,17 @@ interface LoginRequirement
 {
   //--------------------------------------------------------------------------------------------------------------------
   /**
-   * Validates a requirement to a login request and returns the ID of the login response.
+   * Validates a requirement to a login request. Returns:
+   * <ul>
+   * <li> int: The ID of a login response.
+   * <li> null: Indicating no real validation toke place (e.g. a login form is shown only and real validation will be
+   * done when the login form is submitted).
+   * </ul>
    *
    * @param array $data The data for validating the requirement to the login request. This method is allowed to enhance
    *                    this array with new elements.
    *
-   * @return int
+   * @return int|null
    */
   public function validate(&$data);
 
